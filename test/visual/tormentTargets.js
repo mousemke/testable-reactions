471977c1b9ab6e8f9fc385f5ecaabaf037f06e1a
Mouse Braun <jason.joseph.braun@zalando.de>
2018-04-12 12:22:02 +0200

    updated dependencies
    + configured webpack 4
    + niffy => torment

--- test/visual/tormentTargets.js
+++ test/visual/tormentTargets.js
@@ -1,8 +1,8 @@
-const niffyTargets = {
+const tormentTargets = {
   xl: [1205, 600],
   l: [965, 600],
   m: [605, 600],
   s: [325, 600],
 };
 
-export default niffyTargets;
+export default tormentTargets;
--- variables.js
+++ variables.js
@@ -2,8 +2,8 @@
 const variables = {
     DEV_SERVER_HOST : '0.0.0.0',
     DEV_SERVER_PORT : 4080,
-    NIFFY_SERVER_HOST: '0.0.0.0',
-    NIFFY_SERVER_PORT: 4081,
+    TORMENT_SERVER_HOST: '0.0.0.0',
+    TORMENT_SERVER_PORT: 4081,
 };
 
 module.exports = variables;
